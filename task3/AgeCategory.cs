c5922ab1662b63e2bf43813fe5e5ebf81a3be939
karuna12345-code <karuna.raiofficial2023@gmail.com>
2025-11-12 12:10:33 +0545

    third commit

---
+++ task3/AgeCategory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal class AgeCategory
+    {
+        public void CheckAgeCategory()
+        {
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 13)
+            {
+                Console.WriteLine("You are a Child.");
+            }
+            else if (age >= 13 && age <= 19)
+            {
+                Console.WriteLine("You are a Teenager.");
+            }
+            else
+            {
+                Console.WriteLine("You are an Adult.");
+            }
+        }
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AgeCategory ageCategory = new AgeCategory();
+            ageCategory.CheckAgeCategory();
+        }
+    }
+}
