54d2658c1a04de025b11e69d3180c11d47296f88
karuna12345-code <karuna.raiofficial2023@gmail.com>
2025-11-12 11:41:03 +0545

    first commit

---
+++ task1/Operators.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Operators
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine($"Sum: {a + b}");
+        }
+
+        public void Subtract(int a, int b)
+        {
+            Console.WriteLine($"Difference: {a - b}");
+        }
+
+        public void Multiply(int a, int b)
+        {
+            Console.WriteLine($"Product: {a * b}");
+
+        }
+
+        public void Divide(int a, int b)
+        {
+            if (b != 0)
+                Console.WriteLine($"Division Result: {a / (double)b}");
+            else
+                Console.WriteLine("Error: Division by zero is not allowed.");
+        }
+
+        
+        public void OddEvenFinder(int number)
+        {
+            string result = (number % 2 == 0) ? "Even Number" : "Odd Number";
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using task1;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // Create an instance of Operators class
+                Operators op = new Operators();
+
+                // Call all methods
+                op.Add(10, 5);
+                op.Subtract(10, 5);
+                op.Multiply(10, 5);
+                op.Divide(10, 5);
+                op.Divide(10, 0); // To test division by zero
+                op.OddEvenFinder(7);
+                op.OddEvenFinder(8);
+            }
+        }
+    }
+}
